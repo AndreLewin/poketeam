51aec1830cf2627554dfc3fc56795bdf7acae33c
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2023-01-27 11:45:01 +0100

    set up ConfigModule (env var) and TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    // https://stackoverflow.com/questions/52570212/nestjs-using-configservice-with-typeormmodule
+    // https://docs.nestjs.com/techniques/database#async-configuration
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        entities: [],
+        synchronize: true,
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) { }
+
   getHello(): string {
+    // console.log(process.env)
+    // const computerName = this.configService.get<string>("NAME", "default value")
     return 'Hello World!';
   }
 }
